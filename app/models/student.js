5e11c422788e1da4823462068d33556e4901a0a9
Caio Cutrim <kaiocoutrim@gmail.com>
2015-09-02 17:46:35 -0300

    added new paramaters to the students schema

--- app/models/student.js
+++ app/models/student.js
@@ -2,10 +2,26 @@ var mongoose = require("mongoose");
 
 module.exports = function(){
 
-	var studentSchema = mongoose.Schema({
-		name: String
-	});
+	var studentSchema = mongoose.Schema(
+		{  name: 
+			 {  type: String
+       ,  required: true
+		   }
 
+		,  surname : 
+	     {  type: String
+		   ,  required: true
+		   }	
+
+		,  address : 
+	     {  street: String
+		   ,  district: String 
+		   ,  city: String
+		   ,  state: String
+		   ,  telephone: String
+			 }
+	  });
 
 	return mongoose.model("Student", studentSchema);
+
 };
