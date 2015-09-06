233874ce8875c25ba8eeec46575d137bd4acd72b
Caio Cutrim <kaiocoutrim@gmail.com>
2015-09-05 21:31:38 -0300

    added a new interceptor and clearing the code

--- app/routes/student.js
+++ app/routes/student.js
@@ -1,16 +1,15 @@
-
 module.exports = function(app){
 	var studentController = app.controllers.student;
-
-
+  var interceptor = app.services.interceptor;
+	var ensureAuthorized = interceptor.ensureAuthorized;
 	app.route("/students")
-	.get(studentController.findAllStudents) //to list all datas 
-	.post(studentController.saveStudent) //to save data student
+	.get(ensureAuthorized, studentController.findAllStudents) //to list all datas 
+	.post(ensureAuthorized, studentController.saveStudent) //to save data student
 	;
 
 	app.route("/students/:id")
-	.get(studentController.findOneStudent)
-	.delete(studentController.removeStudent)
+	.get(ensureAuthorized, studentController.findOneStudent)
+	.delete(ensureAuthorized, studentController.removeStudent)
 	;
 	
 }
