f5858411f0f12434675f4ede323f7a616ba4a74f
Caio Cutrim <kaiocoutrim@gmail.com>
2015-09-05 21:33:14 -0300

    added morgan and cors function middleware on express configuration

--- config/express.js
+++ config/express.js
@@ -2,6 +2,7 @@ var express = require("express")
 	, methodOverride = require("method-override")
 	, bodyParser = require("body-parser")
 	, load = require("express-load")
+	, morgan = require("morgan")
 ;
 
 module.exports = function(){
@@ -14,8 +15,17 @@ module.exports = function(){
 	app.use(bodyParser.urlencoded({extended:true}));
 	app.use(bodyParser.json());
 	app.use(methodOverride());
+	app.use(morgan("dev"));
+
+	app.use(function(req, res, next){
+		res.setHeader("Acess-Control-Allow-Origin", "*");
+		res.setHeader("Acess-Control-Allow-Methods", "GET", "POST")
+		res.setHeader("Acess-Control-Allow-Headers", "X-Requested-With, content-type, Authorization");
+		next();
+	});
 
 	load("models",{cwd:"app"})
+	.then("services")
 	.then("controllers")
 	.then("routes")
 	.into(app);
