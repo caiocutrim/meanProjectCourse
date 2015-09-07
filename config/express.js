809716fc559db9cb0b6d5185b2625ba34e934d32
Caio Cutrim <kaiocoutrim@gmail.com>
2015-09-07 13:36:46 -0300

    added helmet to improve some security techniques

--- config/express.js
+++ config/express.js
@@ -1,13 +1,14 @@
+"use strict";
 var express = require("express")
 	, methodOverride = require("method-override")
 	, bodyParser = require("body-parser")
 	, load = require("express-load")
 	, morgan = require("morgan")
+	, helmet = require("helmet")
 ;
 
 module.exports = function(){
   var app = express();
-	app.set("port", 3000);
   
 
   //using the express middlewares
@@ -15,14 +16,21 @@ module.exports = function(){
 	app.use(bodyParser.urlencoded({extended:true}));
 	app.use(bodyParser.json());
 	app.use(methodOverride());
-	app.use(morgan("dev"));
-
 	app.use(function(req, res, next){
 		res.setHeader("Acess-Control-Allow-Origin", "*");
 		res.setHeader("Acess-Control-Allow-Methods", "GET", "POST")
 		res.setHeader("Acess-Control-Allow-Headers", "X-Requested-With, content-type, Authorization");
 		next();
 	});
+	app.use(helmet());
+	app.use(helmet.xframe());
+	app.use(helmet.xssFilter());
+	app.use(helmet.nosniff()); // to block the link of another files that don't be .css or .js on the content-type header
+  // settings
+	app.set("port", 3000);
+
+	// to responde with a false information
+	app.use(helmet.hidePoweredBy({ setTo: "PHP 5.5.14"}));
 
 	load("models",{cwd:"app"})
 	.then("services")
