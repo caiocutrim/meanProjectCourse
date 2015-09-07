4cec02ba4e685d274a7e6b691fb2be700aea4b02
Caio Cutrim <kaiocoutrim@gmail.com>
2015-09-07 13:35:57 -0300

    fixed a bug in a interceptor services that blocked the response token

--- app/services/interceptor.js
+++ app/services/interceptor.js
@@ -1,3 +1,4 @@
+"use strict";
 var jwt = require("jwt-simple")
   , secret = "I'm a rockstar, baby! and you don't will discover this";
 
@@ -5,23 +6,23 @@ module.exports = function(app){
   var controller = {};
 	var user = app.models.users;
 
-	controller.ensureAuthorized = function (req, res, next, app){
+	controller.ensureAuthorized = function (req, res, next){
 
-			var token = (req.body && req.body.access_token) || (req.query && req.query.access_token) || req.headers ["x-access-token"];
-
+			var token = req.headers["authorization"] || req.body; 
+			console.log("token em base64 ->" + token)
 			if(token){
 				  try {
 					  var decoded = jwt.decode(token, secret);
 					  console.log("decoding " + decoded);
 
 					  if(decoded.exp <= Date.now()){
-					  	res.json(400, { error: "Access expired, do the login again!"});
+							res.status(401).json({statusText: "Access expired, do the login again!"});
 					  }
 
 					user.findOne({"_id": decoded.iss}, function(err, user){
 						if(err){ 
 					    console.log("user not found!");
-					    res.status(500).json({"message": "User not found with this token!"});
+					    res.status(401).json({statusText: "User not found with this token!"});
 						}
 						req.user = user;
 						console.log(" I found this user "+ req.user);
@@ -30,11 +31,11 @@ module.exports = function(app){
 
 				 }
 			   catch(err){
-				    return res.status(401).json({"message": "Invalid token!"});
+				    return res.status(401).json({statusText: "Invalid token!"});
 				}
 			}
 			else{
-				 res.status(401).json({"message": "Not Authorized!"});
+				 res.status(401).json({statusText: " I'm not get a token.."});
 			}
 	  };
 
