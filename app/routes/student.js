a0ded34e1195bedcc625caac4f3ce82bc2382636
Caio Cutrim <kaiocoutrim@gmail.com>
2015-09-07 13:30:03 -0300

    added interceptor services correctly and fixed the bug of the authentication

--- app/routes/student.js
+++ app/routes/student.js
@@ -1,15 +1,15 @@
+"use strict";
 module.exports = function(app){
 	var studentController = app.controllers.student;
   var interceptor = app.services.interceptor;
-	var ensureAuthorized = interceptor.ensureAuthorized;
 	app.route("/students")
-	.get(ensureAuthorized, studentController.findAllStudents) //to list all datas 
-	.post(ensureAuthorized, studentController.saveStudent) //to save data student
+	.get(interceptor.ensureAuthorized, studentController.findAllStudents) //to list all datas 
+	.post(interceptor.ensureAuthorized, studentController.saveStudent) //to save data student
 	;
 
 	app.route("/students/:id")
-	.get(ensureAuthorized, studentController.findOneStudent)
-	.delete(ensureAuthorized, studentController.removeStudent)
+	.get(interceptor.ensureAuthorized, studentController.findOneStudent)
+	.delete(interceptor.ensureAuthorized, studentController.removeStudent)
 	;
 	
 }
