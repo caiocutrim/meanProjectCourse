756fe20642e2408eafa1c962b69dfa0e2203bb4b
Caio Cutrim <kaiocoutrim@gmail.com>
2015-09-10 14:13:34 -0300

    added email field

--- app/models/student.js
+++ app/models/student.js
@@ -1,26 +1,23 @@
 var mongoose = require("mongoose");
-
 module.exports = function(){
-
-	var studentSchema = mongoose.Schema(
-		{  name: 
-			 {  type: String
-       ,  required: true
-		   }
-
-		,  surname : 
-	     {  type: String
-		   ,  required: true
-		   }	
-
-		,  address : 
-	     {  street: String
-		   ,  district: String 
-		   ,  city: String
-		   ,  state: String
-		   ,  telephone: String
-			 }
-	  });
+	var studentSchema = mongoose.Schema({  
+		name: {  
+			 type: String,
+			 required: true
+		 },
+		 surname : {  
+			 type: String,
+			 required: true
+		 },
+		 address : {  
+			 street: String,
+			 district: String,
+			 city: String,
+			 state: String,
+			 telephone: String,
+			 email: String
+		 }
+	});
 
 	return mongoose.model("Student", studentSchema);
 
